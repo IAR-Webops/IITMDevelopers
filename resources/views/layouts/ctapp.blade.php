7d5588fef3b7e98aa2fc6db001fe77e13ca34926
Yash Murty <yashmurty@gmail.com>
2016-04-23 03:26:32 +0530

    Portfolio Thumbnail description

--- resources/views/layouts/ctapp.blade.php
+++ resources/views/layouts/ctapp.blade.php
@@ -138,7 +138,7 @@
             <div class="row">
                 <div class="col-lg-12 text-center">
                     <h2 class="section-heading">Portfolio</h2>
-                    <h3 class="section-subheading text-muted">Lorem ipsum dolor sit amet consectetur.</h3>
+                    <h3 class="section-subheading text-muted">Projects delivered so far.</h3>
                 </div>
             </div>
             <div class="row">
@@ -152,8 +152,8 @@
                         <img src="img/portfolio/roundicons.png" class="img-responsive" alt="">
                     </a>
                     <div class="portfolio-caption">
-                        <h4>Round Icons</h4>
-                        <p class="text-muted">Graphic Design</p>
+                        <h4>IITM Connect</h4>
+                        <p class="text-muted">Alumni/Student Platform</p>
                     </div>
                 </div>
                 <div class="col-md-4 col-sm-6 portfolio-item">
@@ -166,8 +166,8 @@
                         <img src="img/portfolio/startup-framework.png" class="img-responsive" alt="">
                     </a>
                     <div class="portfolio-caption">
-                        <h4>Startup Framework</h4>
-                        <p class="text-muted">Website Design</p>
+                        <h4>International Portal</h4>
+                        <p class="text-muted">International Internships/Exchange</p>
                     </div>
                 </div>
                 <div class="col-md-4 col-sm-6 portfolio-item">
@@ -180,11 +180,12 @@
                         <img src="img/portfolio/treehouse.png" class="img-responsive" alt="">
                     </a>
                     <div class="portfolio-caption">
-                        <h4>Treehouse</h4>
-                        <p class="text-muted">Website Design</p>
+                        <h4>Linkedin Scraper</h4>
+                        <p class="text-muted">Data Extractor</p>
                     </div>
                 </div>
-                <div class="col-md-4 col-sm-6 portfolio-item">
+
+                <!-- <div class="col-md-4 col-sm-6 portfolio-item">
                     <a href="#portfolioModal4" class="portfolio-link" data-toggle="modal">
                         <div class="portfolio-hover">
                             <div class="portfolio-hover-content">
@@ -225,7 +226,8 @@
                         <h4>Dreams</h4>
                         <p class="text-muted">Website Design</p>
                     </div>
-                </div>
+                </div> -->
+
             </div>
         </div>
     </section>
