16a4548959f0b3560dd1a8bcb4bc476e2e448b61
Yash Murty <yashmurty@gmail.com>
2016-05-01 09:54:24 +0530

    IITMConnect Modal project

--- resources/views/layouts/ctapp.blade.php
+++ resources/views/layouts/ctapp.blade.php
@@ -495,17 +495,16 @@
                     <div class="col-lg-8 col-lg-offset-2">
                         <div class="modal-body">
                             <!-- Project Details Go Here -->
-                            <h2>Project Name</h2>
-                            <p class="item-intro text-muted">Lorem ipsum dolor sit amet consectetur.</p>
-                            <img class="img-responsive img-centered" src="img/portfolio/roundicons-free.png" alt="">
-                            <p>Use this area to describe your project. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Est blanditiis dolorem culpa incidunt minus dignissimos deserunt repellat aperiam quasi sunt officia expedita beatae cupiditate, maiores repudiandae, nostrum, reiciendis facere nemo!</p>
+                            <h2>IITM Connect</h2>
+                            <p class="item-intro text-muted">Connecting Alumni and Students with the Institute.<br>
+                            Visit <a class="btn btn-default" href="http://iitmconnect.iitmadras.in/">IITMConnect</a>
+                            </p>
+                            <img class="img-responsive img-centered" src="img/portfolio/roundicons-free.png" alt="" style="height: 300px">
+                            <p><strong>#iitmconnect</strong> is the first project that has been taken up by the I&AR Web Operations Team.
+                            </p>
                             <p>
-                                <strong>Want these icons in this portfolio item sample?</strong>You can download 60 of them for free, courtesy of <a href="https://getdpd.com/cart/hoplink/18076?referrer=bvbo4kax5k8ogc">RoundIcons.com</a>, or you can purchase the 1500 icon set <a href="https://getdpd.com/cart/hoplink/18076?referrer=bvbo4kax5k8ogc">here</a>.</p>
-                            <ul class="list-inline">
-                                <li>Date: July 2014</li>
-                                <li>Client: Round Icons</li>
-                                <li>Category: Graphic Design</li>
-                            </ul>
+                                The purpose of this project is to help you stay in touch with insti forever.
+                            </p>
                             <button type="button" class="btn btn-primary" data-dismiss="modal"><i class="fa fa-times"></i> Close Project</button>
                         </div>
                     </div>
