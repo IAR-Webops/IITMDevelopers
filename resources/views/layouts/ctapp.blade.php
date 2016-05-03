61b0ba70aeb91e6baf6e5adb0ba82f75e34ee00c
Yash Murty <yashmurty@gmail.com>
2016-05-03 15:11:00 +0530

    URL Target

--- resources/views/layouts/ctapp.blade.php
+++ resources/views/layouts/ctapp.blade.php
@@ -394,7 +394,7 @@
         <div class="container">
             <div class="row">
                 <div class="col-md-4">
-                    <span class="copyright">Copyright &copy; <a href="http://yashmurty.com">Yash Murty</a></span>
+                    <span class="copyright">Copyright &copy; </span><a target="_alt" href="http://yashmurty.com">Yash Murty</a>
                 </div>
                 <div class="col-md-4">
                     <ul class="list-inline social-buttons">
