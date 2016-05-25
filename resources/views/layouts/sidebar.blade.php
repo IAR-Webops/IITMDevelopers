bbd22baf37c1a8773c1c33c365646a27145fc2cd
Yash Murty <yashmurty@gmail.com>
2016-05-25 19:33:32 +0530

    Layout - Sidebar blade

---
+++ resources/views/layouts/sidebar.blade.php
@@ -0,0 +1,23 @@
+<div id="sidebar-wrapper">
+            <ul class="sidebar-nav nav-pills nav-stacked" id="menu">
+                
+                <li class="active">
+                    <a href="{{ url('/home') }}"><span class="fa-stack fa-lg pull-left"><i class="fa fa-home fa-stack-1x "></i></span>Home</a>
+                </li>
+                <li>
+                    <a href="#"> <span class="fa-stack fa-lg pull-left"><i class="fa fa-cloud fa-stack-1x "></i></span>Web Hosting</a>
+                </li>
+                <li>
+                    <a href="#"><span class="fa-stack fa-lg pull-left"><i class="fa fa-globe fa-stack-1x "></i></span>Domain Registration</a>
+                </li>
+                <li>
+                    <a href="#"><span class="fa-stack fa-lg pull-left"><i class="fa fa-key fa-stack-1x "></i></span>IITM Connect Oauth</a>
+                </li>
+                <li>
+                    <a href="#"><span class="fa-stack fa-lg pull-left"><i class="fa fa-envelope fa-stack-1x "></i></span>Contact</a>
+                </li>
+                <li>
+                    <a href="{{ url('/logout') }}"><span class="fa-stack fa-lg pull-left"><i class="fa fa-sign-out fa-stack-1x "></i></span>Logout</a>
+                </li>
+            </ul>
+        </div>
